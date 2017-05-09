b93f8699c55d90714c1f999ed0b29a283e67881c
pmprasinos <pmprasinos@gmail.com>
2017-05-09 06:01:22 -0500

    Update for speed
    Transmission rate reduced to 15-35ms

--- ArduinoMaster.ino
+++ ArduinoMaster.ino
@@ -44,10 +44,11 @@ int bluetoothcounter = 0; //This increases when nothing is sent to the contoller
 int previousbtc = 0;
 int ppbtc = 0;
 int screencount = 0;
+int loopcount = 0;
 
 int btcount = 101;
 char CurrentScreenState = '0';
-
+int bluetoothread;
 int notconnected = 0;
 
 void setup() {
@@ -55,21 +56,25 @@ void setup() {
 
   int previousbtc = 0;
   int btc = 0;
- 
+
 
   Wire.begin();
-  Serial.begin(9600);
+  Wire.beginTransmission(1);
+  //Serial.begin(9600);
   bluetooth.begin(19200);
-
+  //Serial.println(loopcount);
   pinMode(Sync, INPUT);
   pinMode(Deadman, INPUT);
   pinMode(Zaxis, INPUT);
   pinMode(Estop, INPUT);
   pinMode(indicatorled, OUTPUT);
   delay (5000);
+  //bluetooth.listen();
 }
-void loop() {
 
+void loop() {
+  loopcount++;
+  ////Serial.println(loopcount);
 
 
   bluetoothcounter = 0;
@@ -81,65 +86,70 @@ void loop() {
   }
 
 
-  int bluetoothread = bluetooth.read();
+  
+    bluetoothread = bluetooth.read();
+   // if(bluetoothread != -1) Serial.println(bluetoothread);
 
-  if (bluetoothread != -1) {
-    btcount = 0;
-  }
-  if (bluetoothread == -1) {
-    btcount ++;
+    ////Serial.println(loopcount);
 
-    if (btcount == 100 ) {
-      Wire.beginTransmission(1);
-      Wire.write('H');
-      Wire.endTransmission();
-      CurrentScreenState = 'H';
-
+    if (bluetoothread != -1) {
+      btcount = 0;
     }
-  }
+    if (bluetoothread == -1) 
+    {
+      btcount ++;
 
-  if (bluetoothread != previousbtc && bluetoothread < 100 || bluetoothread == -1  ) {
-    previousbtc = bluetoothread;
-    screencount = 0;
-  }
-  if (bluetoothread == previousbtc && bluetoothread < 100 && bluetoothread > -1 ) {
+      if (btcount == 100 ) 
+      {
 
-    previousbtc = bluetoothread;
-    screencount ++;
-  }
+        Wire.write('H');
+        CurrentScreenState = 'H';
+      }
+    }
 
-
-
-
-
-
-
-  if (screencount == 3 && bluetoothread < 100) {
-
-    if (bluetoothread == ppbtc) {
+    if (bluetoothread != previousbtc && bluetoothread < 100 || bluetoothread == -1  ) 
+    {
+      previousbtc = bluetoothread;
       screencount = 0;
-    } else {
+    }
+    if (bluetoothread == previousbtc && bluetoothread < 100 && bluetoothread > -1 ) 
+    {
+     // Serial.println(bluetoothread);
+      previousbtc = bluetoothread;
+      screencount ++;
+    }
 
-      int btc = bluetoothread;
 
-         Wire.beginTransmission(1);
+    if (screencount == 3 && bluetoothread < 100) 
+    {
+
+      if (bluetoothread == ppbtc) {
+        screencount = 0;
+      } else {
+
+        int btc = bluetoothread;
+
+
         Wire.write(btc);
-        Wire.endTransmission();
-        if (btc == 'E' ||btc == 'F')
-        {}else{
+        
+        if (btc == 'E' || btc == 'F')
+        {} else {
           CurrentScreenState = btc;
         }
-    
-      ppbtc = bluetoothread;
-      previousbtc = bluetoothread;
+
+        ppbtc = bluetoothread;
+        previousbtc = bluetoothread;
+      }
+
+
     }
 
+  
 
-  }
-
+  ////Serial.println(loopcount);
   //////////////////////Joy Stick Read/////////////////////////////
   String vertical, horizontal ;   // read all values from the joystick
- 
+
   vertical = ((map (analogRead(VERT), 0, 1023, 1023, 0) + 10000)); // will be 0-1023
   horizontal = (analogRead(HORIZ) + 10000); // will be 0-1023
 
@@ -150,85 +160,53 @@ void loop() {
   char ZaxisState; //= digitalRead(Zaxis);
   char EstopState; //= digitalRead(Estop);
   char CountState;
-  if (digitalRead(Deadman) == 1) {
+    int CheckVal; 
+  CheckVal = digitalRead(Deadman);
+  
+  if (CheckVal == 1) {
     DeadmanState = 'F';
-  } else if (digitalRead(Deadman) == 0) {
+  } else if (CheckVal == 0) {
     DeadmanState = 'N';
   }
-  if (digitalRead(Zaxis) == 1) {
+  CheckVal = digitalRead(Zaxis);
+  if (CheckVal == 1) {
     ZaxisState = 'F';
-  } else if (digitalRead(Zaxis) == 0) {
+  } else if (CheckVal == 0) {
     ZaxisState = 'N';
   }
-  if (digitalRead(Estop) == 1) {
+  CheckVal = digitalRead(Estop);
+  if (CheckVal == 1) {
     EstopState = 'N';
-  } else if (digitalRead(Estop) == 0) {
+  } else if (CheckVal == 0) {
     EstopState = 'F';
   }
-  if (count == 1) {
+   if (count == 1) {
     CountState = 'N';
   } else if (count == 0) {
     CountState = 'F';
   }
+  ////Serial.println(loopcount);
 
 
-
-
-
+  //delay(1);
   /////////////Toggle Bit///////////////////////////
 
 
+  ////Serial.println(loopcount);
   ///////////////////////////////////////PrintValues/////////////////////
+
+
 
   String packet = String("V" + vertical + "v" + "H" + horizontal + "h" + "Z" + ZaxisState + "z" + "D" + DeadmanState + "d" + "E" + EstopState + "e" + "C" + CountState + "c" + "J" + controllername + "j" + "S" + CurrentScreenState + "s");
   bluetooth.println(packet);
 
+   Wire.endTransmission();
 
-
-
-  ///////////////Battery indicator.//////////////////////////////////////////////
-
-  //battVolts = getBandgap(); 
+        Wire.beginTransmission(1);
 
 }
 
 
 ////////////////////////////////////END OF LOOP////////////////////////////////
-
-
-/////////////////////Battery Checking Function//////////////////////////////
-
-int getBandgap(void) // Returns actual value of Vcc (x 100)
-{
-
-
-
-  // For 168/328 boards
-  const long InternalReferenceVoltage = 1056L;  // Adjust this value to your boards specific internal BG voltage x1000
-  // REFS1 REFS0          --> 0 1, AVcc internal ref. -Selects AVcc external reference
-  // MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)         -Selects channel 14, bandgap voltage, to measure
-  ADMUX = (0 << REFS1) | (1 << REFS0) | (0 << ADLAR) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0);
-
-
-  delay(50);  // Let mux settle a little to get a more stable A/D conversion
-  // Start a conversion
-  ADCSRA |= _BV( ADSC );
-  // Wait for it to complete
-  while ( ( (ADCSRA & (1 << ADSC)) != 0 ) );
-  // Scale the value
-  int results = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L; // calculates for straight line value
-  return results;
-
-
-}
-
-/*void State(void){
-
-  char currentState = Wire.read();
-  Serial.println (currentState);
-  }*/
-
-
-
 
 
