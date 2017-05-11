342a7594dde3e66b103f5f80f258c9df1311c221
pmprasinos <pmprasinos@gmail.com>
2017-05-11 09:14:54 -0500

    Code adds self calibration and ignores non-screen commands
    Highest performing code with the same output string.  Moving now to
    shortened string

--- ArduinoMaster/ArduinoMaster.ino
+++ ArduinoMaster/ArduinoMaster.ino
@@ -4,17 +4,14 @@
 #include <SPI.h>
 #include <Wire.h>
 
-
-
 /////////////////////////Controller Name//////////////////////////////////
 
-char controllername[5] = {'0', '0', '0', '7'} ; // To be changed with every controller.
+char controllername[5] = {'0', '0', '2', '6'} ; // To be changed with every controller.
 
 ///////////////////////////Serial Declaration//////////////////////////////
 int bluetoothTx = 2;  // TX-O pin of bluetooth mate
 int bluetoothRx = 3;  // RX-I pin of bluetooth mate
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
-
 
 ///////////////////Inputs//////////////////////////
 
@@ -25,183 +22,181 @@ SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 #define Estop  5
 #define Zaxis  6
 #define Sync  7
-//const int AccelX = A1;
-//const int AccelY = A2;
 #define AccelZ  A3
+
 
 int indicatorled = 13;
 //////////////////Global Variables/////////////////
+int ShapeStateDeadMan = 0;  //for smaller objects like Estop, and interlocks
+int ShapeStatePin = 0;
+int ShapeStateCarrier = 0;
+
+int hMaxValueCalib = 900; int hMinValueCalib = 100;//put initial calibration values inside expected range of 0-1024
+int vMaxValueCalib = 900; int vMinValueCalib = 100;
+
 
 int battVolts;   // made global for wider avaliblity throughout a sketch if needed, example a low voltage alarm, etc
 int DeadmanState = 0;
 int SyncState = 0 ;
 int ZaxisState = 0;
 int EstopState = 0;
-int upperlimit = 1; //Counter for Toggle bit
-int lowerlimit = 0; //Counter for Toggle bit
-int count; // for the Toggle bit
-int bluetoothcounter = 0; //This increases when nothing is sent to the contoller and resets when a command is recieved. Used to test connection.
-int previousbtc = 0;
-int ppbtc = 0;
-int screencount = 0;
-int loopcount = 0;
 
-int btcount = 101;
-char CurrentScreenState = '0';
-int bluetoothread;
-int notconnected = 0;
+int previousbtc = 0; //Previous bluetooth command
 
+int btTryCount = 1001; //This increases when nothing is sent to the contoller and resets when a command is recieved. Used to test connection.
+int CurrentScreenState = 0;
+int btc;
+int loopcount=0;
+
+
+bool debug = true;
 void setup() {
-
-
-  int previousbtc = 0;
-  int btc = 0;
-
-
+ 
   Wire.begin();
   Wire.beginTransmission(1);
-  Serial.begin(9600);
+  if (debug)Serial.begin(9600);
   bluetooth.begin(19200);
-  Serial.println(loopcount);
   pinMode(Sync, INPUT);
   pinMode(Deadman, INPUT);
   pinMode(Zaxis, INPUT);
   pinMode(Estop, INPUT);
   pinMode(indicatorled, OUTPUT);
-  delay (5000);
+    delay (3000);
+  //  CurrentScreenState = bluetooth.read();
   //bluetooth.listen();
 }
 
 void loop() {
-  loopcount++;
-  ////Serial.println(loopcount);
+loopcount ++;
+if(loopcount %100 == 5) Serial.println(loopcount);
+  bluetooth.listen();
+  btc = bluetooth.read();
 
-
-  bluetoothcounter = 0;
-  if (count < upperlimit) {
-    (count ++);
-  }
-  else if (count > lowerlimit) {
-    (count --);
-  }
-
-
-  bluetooth.listen()
-  bluetoothread = bluetooth.read();
-  if (bluetoothread < 100) bluetoothread = bluetooth.read();
-
-  ////Serial.println(loopcount);
-
-  if (bluetoothread != -1) {
-    btcount = 0;
-  }
-  if (bluetoothread == -1)
+  if (btc == -1)
   {
-    btcount ++;
+    if (btTryCount < 10000) btTryCount ++;
+  }
+  else
+  {
+      
+    btTryCount = 0;
+  }
 
-    if (btcount == 100 )
+  if (btTryCount == 10000 )
+  {
+    if (debug)Serial.println(btc);
+    Wire.write(72);
+    btTryCount ++;
+   }
+
+
+if (btc != previousbtc && btc > 47 && btc < 100 ) //if the character has changed store it to check for doubles on next loop
+{
+     previousbtc = btc; 
+}
+else if (btc == previousbtc && btc < 100 && btc > 47) //if the same character is read twice, and it is between 2 and 100
+{ 
+   
+    if( (btc == 69 || btc == 70))
     {
-      Wire.beginTransmission(1);
-      Wire.write('H');
-      CurrentScreenState = 'H';
+        if (ShapeStateDeadMan != btc) {Wire.write(btc); if (debug)Serial.print("DEADMAN: "); if (debug)Serial.println(btc);}
+        ShapeStateDeadMan = btc;  //for smaller objects like Estop, and interlocks
     }
-  }
-
-  if (bluetoothread != previousbtc && bluetoothread < 100 || bluetoothread == -1  )
-  {
-    previousbtc = bluetoothread;
-    screencount = 0;
-  }
-  if (bluetoothread == previousbtc && bluetoothread < 100 && bluetoothread > -1 )
-  {
-    // Serial.println(bluetoothread);
-    previousbtc = bluetoothread;
-    screencount ++;
-  }
-
-
-  if (screencount == 3 && bluetoothread < 100)
-  {
-
-    if (bluetoothread == ppbtc) {
-      screencount = 0;
-    } else {
-
-      int btc = bluetoothread;
-      Serial.println(bluetoothread);
-      Wire.beginTransmission(1);
-      Wire.write(btc);
-
-      if (btc == 'E' || btc == 'F')
-      {} else {
-        CurrentScreenState = btc;
-      }
-
-      ppbtc = bluetoothread;
-      previousbtc = bluetoothread;
+    else if ( (btc < 69 && btc >  64))
+    {
+        if (ShapeStatePin != btc) {Wire.write(btc); if (debug)Serial.print("    PIN: "); if (debug)Serial.println(btc);}
+        ShapeStatePin = btc;  //for smaller objects like Estop, and interlocks
     }
-
-
-  }
-
-
-
-  ////Serial.println(loopcount);
-  //////////////////////Joy Stick Read/////////////////////////////
-  String vertical, horizontal ;   // read all values from the joystick
-
-  vertical = ((map (analogRead(VERT), 0, 1023, 1023, 0) + 10000)); // will be 0-1023
-  horizontal = (analogRead(HORIZ) + 10000); // will be 0-1023
-
-
-
-  ///////////////////// Button State Read////////////////////////
-  char DeadmanState; //= digitalRead(Deadman);
-  char ZaxisState; //= digitalRead(Zaxis);
-  char EstopState; //= digitalRead(Estop);
-  char CountState;
-  int CheckVal;
-  CheckVal = digitalRead(Deadman);
-
-  if (CheckVal == 1) {
-    DeadmanState = 'F';
-  } else if (CheckVal == 0) {
-    DeadmanState = 'N';
-  }
-  CheckVal = digitalRead(Zaxis);
-  if (CheckVal == 1) {
-    ZaxisState = 'F';
-  } else if (CheckVal == 0) {
-    ZaxisState = 'N';
-  }
-  CheckVal = digitalRead(Estop);
-  if (CheckVal == 1) {
-    EstopState = 'N';
-  } else if (CheckVal == 0) {
-    EstopState = 'F';
-  }
-  if (count == 1) {
-    CountState = 'N';
-  } else if (count == 0) {
-    CountState = 'F';
-  }
-  ////Serial.println(loopcount);
-
-
-  //delay(1);
-  /////////////Toggle Bit///////////////////////////
-
-
-  ////Serial.println(loopcount);
-  ///////////////////////////////////////PrintValues/////////////////////
-
-  Wire.endTransmission();
-
-  String packet = String("V" + vertical + "v" + "H" + horizontal + "h" + "Z" + ZaxisState + "z" + "D" + DeadmanState + "d" + "E" + EstopState + "e" + "C" + CountState + "c" + "J" + controllername + "j" + "S" + CurrentScreenState + "s");
-  bluetooth.println(packet);
+    else if ( (btc > 49 && btc <73 ) && (btc <55 || btc > 65))
+    {
+        //if (debug)Serial.println(btc);
+        if(CurrentScreenState != btc) 
+        { 
+            Wire.write(btc); 
+            if(debug)Serial.println(btc);
+            CurrentScreenState = btc; 
+            if(debug)Serial.print("CURRENT SCREEN: ");
+            if(debug)Serial.println(CurrentScreenState);
+        }
+          
+     }
+     
+      previousbtc = btc; 
+   
+}
 
 
 
+
+////Serial.println(loopcount);
+//////////////////////Joy Stick Read/////////////////////////////
+String vertical, horizontal ;   // read all values from the joystick
+
+vertical = analogRead(VERT); // will be 0-1023
+horizontal = analogRead(HORIZ); // will be 0-1023
+
+//Adjust min & max for in line calibration
+if (vertical.toInt() < vMinValueCalib ) vMinValueCalib = vertical.toInt() + 1;
+if (vertical.toInt() > vMaxValueCalib) vMaxValueCalib = vertical.toInt() - 1;
+if (horizontal.toInt() < hMinValueCalib) hMinValueCalib = horizontal.toInt() + 1;
+if (horizontal.toInt() > hMaxValueCalib) hMaxValueCalib = horizontal.toInt() - 1;
+
+if ( false && loopcount % 40 ==5  )
+{
+  Serial.print("hMIN: " );  Serial.print(hMinValueCalib );  Serial.print("   hMAX: " );  Serial.print( hMaxValueCalib);  Serial.print("   hRAW:" + horizontal);
+  Serial.print("    vMIN: " );  Serial.print(vMinValueCalib );  Serial.print("   vMAX: " );  Serial.print( vMaxValueCalib);  Serial.println("   vRAW:" + vertical);
+}
+
+
+vertical = (map (vertical.toInt(), vMinValueCalib, vMaxValueCalib, 1, 1023) ) + 10000; // will be 10-1010
+horizontal = (map(horizontal.toInt(), hMinValueCalib, hMaxValueCalib, 1, 1023)+ 10000 )  ; // will be 10-1010
+
+
+///////////////////// Button State Read////////////////////////
+char DeadmanState = '0'; //= digitalRead(Deadman);
+char ZaxisState= '0'; //= digitalRead(Zaxis);
+char EstopState= '0'; //= digitalRead(Estop);
+char CountState= '0';
+int CheckVal;
+
+CheckVal = digitalRead(Deadman);
+if (CheckVal == 1) {
+  DeadmanState = 'F';
+} else if (CheckVal == 0) {
+  DeadmanState = 'N';
+}
+CheckVal = digitalRead(Zaxis);
+if (CheckVal == 1) {
+  ZaxisState = 'F';
+} else if (CheckVal == 0) {
+  ZaxisState = 'N';
+}
+CheckVal = digitalRead(Estop);
+if (CheckVal == 1) {
+  EstopState = 'N';
+} else if (CheckVal == 0) {
+  EstopState = 'F';
+}
+ if(CountState == 'N') CountState = 'F';
+ else CountState = 'N';
+
+///////////////////////////////////////PrintValues/////////////////////
+//CurrentScreenState = 72;
+char CurrentScreenChar = (char)CurrentScreenState + '0';
+
+String CurrentScreenString = String(CurrentScreenChar);
+CurrentScreenString.toUpperCase();
+
+Serial.print(CurrentScreenState);
+Serial.print(CurrentScreenChar);
+
+String packet = String("V" + vertical + "v" + "H" + horizontal + "h" + "Z" + ZaxisState + "z" + "D" + DeadmanState + "d" + "E" + EstopState + "e" + "C" + CountState + "c" + "J" + controllername + "j" + "S" + CurrentScreenString + "s");
+bluetooth.println(packet);
+
+
+Wire.endTransmission();
+if(debug)Serial.println(packet);
+Wire.beginTransmission(1);
 
 
 }
